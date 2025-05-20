29fea5edd2463e3caabc3bb2c33c329e91f4c57f
HUANG <starnightsola@gmail.com>
2025-05-20 16:28:26 +0900

    Expressサーバー起動

---
+++ src/index.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const app = express()
+
+app.get('/', (req, res) => {
+  res.send('Hello from Express!')
+})
+
+const PORT = 3001
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`)
+})
