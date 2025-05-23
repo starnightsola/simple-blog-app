e3ade87534feb574dcd3a50262e4a557dc7a3e46
HUANG <starnightsola@gmail.com>
2025-05-23 14:04:06 +0900

    新しい記事を追加！記事作成API (POST) を作る

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,8 +1,12 @@
 const express = require('express')
 const postsRouter = require('./routes/posts')
+
 const app = express()
 
+// ⭐️ この1行を追加！リクエストの JSON をパースできるようにする
 app.use(express.json())
+
+// ルーティング
 app.use('/api/posts', postsRouter)
 
 app.listen(3000, () => {
--- backend/src/routes/posts.js
+++ backend/src/routes/posts.js
@@ -20,7 +20,8 @@ router.get('/',(req,res) => {
 
 // 個別記事取得
 router.get('/:id', (req, res) => {
-  db.get('SELECT * FROM posts WHERE id = ?', [postID], (err, row) => {
+  const postId = req.params.id
+  db.get('SELECT * FROM posts WHERE id = ?', [postId], (err, row) => {
     if (err) {
       return res.status(500).json({ error: 'DBエラー'})// ← SQL実行失敗
     }
@@ -30,5 +31,48 @@ router.get('/:id', (req, res) => {
     res.json(row)// ← 成功したら JSONで記事データを返す
   })
 })
+
+// 新しい記事を追加するAPI（POST /api/posts）
+// /api/posts` に対する **POSTリクエスト**（記事追加）を処理するルート定義。
+router.post('/',(req, res) => {
+  const { title, content } = req.body
+
+  // 入力チェック
+  // どちらかが空だったらエラー（`400 Bad Request`）を返す。
+  if(!title || !content) {
+    return res.status(400).json({ error: 'タイトルと本文は必須です'})// ← 
+  }
+
+  // new Date():JavaScriptで現在の日付と時刻を取得します。
+  // 例：2025-05-22T18:23:45.123Z（これは ISO 8601形式）
+  // .toISOString():その日付を「文字列」に変換します（ISOフォーマット）
+  const createdAt = new Date().toISOString()
+
+  // 新しい記事を作るときには、作成日時（createdAt）＝今の時刻
+  // 更新日時（updatedAt）＝最初は同じ（あとで編集されたら更新される）
+  const updatedAt = createdAt
+
+  const sql = 'INSERT INTO posts (title, content, createdAt, updatedAt) VALUES (?, ?, ?, ?)'
+
+  const params = [title, content, createdAt, updatedAt]
+
+  db.run(sql, params, function (err) {
+    if (err) {
+      return res.status(500).json({ error: 'データベースエラー'})
+    }
+    // 成功時は、追加された記事のIDなどを返す
+    res.status(201).json({
+      message: '記事が作成されました',
+      post: {
+        id: this.lastID,
+        title,
+        content,
+        createdAt,
+        updatedAt
+      }
+    })
+  })
+})
+
 // このファイルの外に router を渡す（export する）
 module.exports = router
