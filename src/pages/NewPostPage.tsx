722605dfa863447bd240d9ab622c250a757a700f
HUANG <starnightsola@gmail.com>
2025-05-26 14:50:48 +0900

    ページコンポーネントの作成

--- src/pages/NewPostPage.tsx
+++ src/pages/NewPostPage.tsx
@@ -1,10 +1,45 @@
-import { Box, Heading, Text} from '@chakra-ui/react'
+import { Box, FormControl, FormLabel, Heading, VStack, Textarea, Button} from '@chakra-ui/react'
+import { useState } from 'react'
 
 const NewPostPage = () => {
+    // 入力されたタイトルと本文の状態を管理
+    const [title, setTitle] = useState('')
+    const [content, setContent] = useState('')
+
+    //　フォームの送信状態（仮）
+    const handleSubmit = (e: React.FormEvent) => {
+        e.preventDefault()
+        console.log('タイトル：', title)
+        console.log('本文', content)
+        // あとでここにAPI連携を追加する
+    }
     return (
-        <Box>
-            <Heading mb={4}>新規記事作成</Heading>
-            <Text>ここに投稿フォームが入る</Text>
+        <Box maxW="600px" mx="auto">
+            <Heading mb={6}>新規記事作成</Heading>
+            <form onSubmit={handleSubmit}>
+                <VStack spacing={4} align="stretch">
+                    <FormControl>
+                        <FormLabel>タイトル</FormLabel>
+                        <input
+                            value={title}
+                            onChange={(e) => setTitle(e.target.value)}
+                            placeholder='記事のタイトル'
+                        />
+                    </FormControl>
+                    <FormControl>
+                        <FormLabel>本文</FormLabel>
+                        <Textarea
+                            value={content}
+                            onChange={(e) => setContent(e.target.value)}
+                            placeholder='本文を入力'
+                            rows={6}
+                        />
+                    </FormControl>
+                    <Button type='submit' colorScheme='blue'>
+                        投稿する
+                    </Button>
+                </VStack>
+            </form>
         </Box>
     )
 }
